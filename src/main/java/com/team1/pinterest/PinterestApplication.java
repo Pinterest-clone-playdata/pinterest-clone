cab4f6da3f6048f50524cb34e6027e2cc9906a0b
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-03-04 14:29:14 +0900

    first

---
+++ src/main/java/com/team1/pinterest/PinterestApplication.java
@@ -0,0 +1,13 @@
+package com.team1.pinterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinterestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team1/pinterest/PinterestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.pinterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
