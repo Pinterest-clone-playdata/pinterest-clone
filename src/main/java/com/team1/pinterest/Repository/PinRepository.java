f31e7006b6ea68dd6819e9d4e6a964e71eb427f0
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-03-14 22:58:43 +0900

    Pin Read

--- src/main/java/com/team1/pinterest/Repository/PinRepository.java
+++ src/main/java/com/team1/pinterest/Repository/PinRepository.java
@@ -2,13 +2,16 @@ package com.team1.pinterest.Repository;
 
 import com.team1.pinterest.Entitiy.Pin;
 import com.team1.pinterest.Entitiy.User;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PinRepository extends JpaRepository<Pin, Long> {
+public interface PinRepository extends JpaRepository<Pin, Long>, PinRepositoryCustom {
 
     boolean existsByTitle(String title);
 
     Optional<Pin> findByIdAndUser(Long id, User user);
+
+    
 }
