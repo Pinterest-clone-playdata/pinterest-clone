acc51f29bb3ea0e0a213a5fbea68310883087802
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-03-23 13:36:42 +0900

    API tempId Remove

--- src/main/java/com/team1/pinterest/Controller/CommentLikeAPI.java
+++ src/main/java/com/team1/pinterest/Controller/CommentLikeAPI.java
@@ -4,6 +4,7 @@ import com.team1.pinterest.DTO.Basic.ResponseDTO;
 import com.team1.pinterest.Service.CommentLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,17 +15,17 @@ public class CommentLikeAPI {
     private final CommentLikeService commentLikeService;
 
     @PostMapping("comment-like/{commentId}")
-    public ResponseEntity<?> createCommentLike(@PathVariable("commentId") Long commentId){
-        Long tempUserId = 1L;
-        commentLikeService.createCommentLike(tempUserId,commentId);
+    public ResponseEntity<?> createCommentLike(@PathVariable("commentId") Long commentId, @AuthenticationPrincipal Long userId){
+
+        commentLikeService.createCommentLike(userId,commentId);
         ResponseDTO<Object> response = ResponseDTO.builder().status(200).message("commentLike add success").build();
         return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("comment-like/{commentId}")
-    public ResponseEntity<?> deleteCommentLike(@PathVariable("commentId") Long commentId){
-        Long tempUserId = 1L;
-        commentLikeService.deleteCommentLike(tempUserId,commentId);
+    public ResponseEntity<?> deleteCommentLike(@PathVariable("commentId") Long commentId, @AuthenticationPrincipal Long userId){
+
+        commentLikeService.deleteCommentLike(userId,commentId);
         ResponseDTO<Object> response = ResponseDTO.builder().status(200).message("commentLike remove success").build();
         return ResponseEntity.ok().body(response);
     }
--- src/main/java/com/team1/pinterest/Security/SecurityConfig.java
+++ src/main/java/com/team1/pinterest/Security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/v1/auth/**").permitAll()
                 .anyRequest()
                 .authenticated();
 
